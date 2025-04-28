990dc0aaab3b1c10284d03eedf4be10583301796
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2025-04-28 09:43:34 -0300

    create projeto MotoConnct

---
+++ moto-connect-api/src/main/java/br/com/fiap/moto_connect_api/MotoConnectApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.moto_connect_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoConnectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoConnectApiApplication.class, args);
+	}
+
+}
---
+++ moto-connect-api/src/test/java/br/com/fiap/moto_connect_api/MotoConnectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.moto_connect_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotoConnectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
